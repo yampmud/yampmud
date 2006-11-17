43aebcb6c28527447fc186552c12434e91278ecc
Bobby Bailey <nosanity79@gmail.com>
2006-11-17 03:34:23 +0000

    removed TRADITIONAL support for old non-ansi compilers

--- src/merc.h
+++ src/merc.h
@@ -34,21 +34,10 @@
 #include <stdbool.h>
 
 
-/*
- * Accommodate old non-Ansi compilers.
- */
-#if defined(TRADITIONAL)
-#define const
-#define args( list )			( )
-#define DECLARE_DO_FUN( fun )		void fun( )
-#define DECLARE_SPEC_FUN( fun )		bool fun( )
-#define DECLARE_SPELL_FUN( fun )	void fun( )
-#else
 #define args( list )			list
 #define DECLARE_DO_FUN( fun )		DO_FUN    fun
 #define DECLARE_SPEC_FUN( fun )		SPEC_FUN  fun
 #define DECLARE_SPELL_FUN( fun )	SPELL_FUN fun
-#endif
 
 /*
  * Short scalar types.
